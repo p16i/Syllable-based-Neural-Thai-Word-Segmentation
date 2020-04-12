702243374a2476caa3c950641b10f86e60e2bd47
heytitle <pat.chormai@gmail.com>
2020-04-12 22:40:55 +0200

    Revert "some optimization for stats calculation"
    This reverts commit 3be2236c260ff7dce679a61b76b6cb5bed172bd6.

--- attacut/evaluation.py
+++ attacut/evaluation.py
@@ -1,28 +1,33 @@
 from collections import namedtuple
 
-import torch
-
 import numpy as np
 
 from nptyping import Array
 
 EvaluationMetrics = namedtuple(
     "EvaluationMetrics",
-    ["tp", "fp", "fn"]
+    ["tp", "fp", "fn", "precision", "recall", "f1"]
 )
 
 def compute_metrics(
-    labels: Array[torch.int],
-    preds: Array[torch.int]
+    labels: Array[np.int32],
+    preds: Array[np.int32]
 ) -> EvaluationMetrics:
 
     # manually implemented due to keep no. of dependencies minimal
-    tp = torch.sum(preds * labels)
-    fp = torch.sum(preds * (1-labels))
-    fn = torch.sum((1-preds) * labels)
+    tp = np.sum(preds * labels)
+    fp = np.sum(preds * (1-labels))
+    fn = np.sum((1-preds) * labels)
+
+    precision = tp / (tp+fp)
+    recall = tp / (tp+fn)
+    f1 = 2 * precision * recall / (precision + recall)
 
     return EvaluationMetrics(
-        tp=tp.cpu(),
-        fp=fp.cpu(),
-        fn=fn.cpu(),
+        tp=tp,
+        fp=fp,
+        fn=fn,
+        precision=precision,
+        recall=recall,
+        f1=f1
     )
