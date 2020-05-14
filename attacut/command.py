4dcf7b90c16b9a41bca93bf2db73be95cb32f0ff
heytitle <pat.chormai@gmail.com>
2020-05-14 22:10:32 +0200

    add eval and compute stat script

---
+++ attacut/command.py
@@ -0,0 +1,141 @@
+import sys
+import os
+import time
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+
+from attacut import Tokenizer, __version__, preprocessing, utils, models
+
+# from https://github.com/pytorch/pytorch/issues/1494#issuecomment-305993854
+from multiprocessing import set_start_method
+try:
+    set_start_method('spawn')
+except RuntimeError:
+    pass
+
+SEP = "|"
+
+def get_argument(dict, name, default):
+    v = dict.get(name)
+    return v if v is not None else default
+
+class AttaCutCLIDataset(Dataset):
+      def __init__(self, src, tokenizer, device):
+          self.src = src
+          self.total_lines = utils.wc_l(self.src)
+
+          self.tokenizer = tokenizer
+
+          self.device = device
+
+          self.data = []
+          with open(self.src, "r") as fin:
+              for txt in fin:
+                txt = preprocessing.TRAILING_SPACE_RX.sub("", txt)
+
+                tokens, features = self.tokenizer.dataset.make_feature(txt)
+
+                inputs = (
+                    features,
+                    torch.zeros(features[1])  # dummy label when won't need it here
+                )
+
+                (x, seq), labels, _= self.tokenizer.dataset.prepare_model_inputs(
+                  inputs,
+                )
+
+                x = torch.squeeze(x)
+
+                self.data.append((((x, seq), labels), tokens))
+
+      def __len__(self):
+          return self.total_lines
+
+      def __getitem__(self, idx):
+          return self.data[idx]
+
+def collate_fn(tokenizer, batch, device):
+    inputs, tokens = [], []
+
+    for x, t in batch:
+      inputs.append(x)
+      tokens.append(t)
+
+    (x, seq), labels, perm_idx = tokenizer.dataset.collate_fn(inputs)
+
+    return ((x.to(device), seq.to(device)), labels, perm_idx), tokens
+
+def main(src, model, num_cores=4, batch_size=32, dest=None, device="cpu"):
+
+    assert num_cores >= 0, "Input given to <num-thread> should greather than or equal one"
+
+    if not src:
+      print(__doc__)
+      sys.exit(0)
+
+    # for a custom model, use the last dir's name.
+    model_name = model.split("/")[-1]
+
+    dest = dest if dest is not None else utils.add_suffix_to_file_path(src, f"tokenized-by-{model_name}")
+
+    print(f"Tokenizing {src}")
+    print(f"Using {src}")
+    print(f"Output: {dest}")
+
+    tokenizer = Tokenizer(model)
+
+    total_lines = utils.wc_l(src)
+
+    if num_cores == 0:
+        print(f"Use main process processing for {total_lines} lines")
+    else:
+        print(f"Use {num_cores} cores for processing for {total_lines} lines")
+
+    print(f"device={device}")
+
+    start_time = time.time()
+    ds = AttaCutCLIDataset(src, tokenizer, device)
+    dataloader = DataLoader(
+      ds,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=0, # only use main process
+      collate_fn=lambda batch: collate_fn(tokenizer, batch, device)
+    )
+
+    tokenizer.model.to(device)
+
+    results = []
+
+    with torch.no_grad(), \
+        tqdm(total=total_lines) as tq, \
+        open(dest, "w") as fout:
+          for batch in dataloader:
+              (x, labels, perm_idx), tokens = batch
+              probs = tokenizer.model(x).cpu().detach().numpy()
+
+              preds = tokenizer.model.output_scheme.decode_condition(
+                np.argmax(probs, axis=2)
+              )
+
+              max_seq = x[1].max().cpu().detach().numpy()
+
+              perm_idx = perm_idx.cpu().detach()
+              preds = preds.reshape((-1, max_seq))
+
+              for ori_ix, after_sorting_ix in enumerate(np.argsort(perm_idx)):
+                  pred = preds[after_sorting_ix, :]
+                  token = tokens[ori_ix]
+
+                  words = preprocessing.find_words_from_preds(token, pred)
+                  fout.write("%s\n" % SEP.join(words))
+
+              tq.update(n=preds.shape[0])
+
+    time_took = time.time() - start_time
+
+    return time_took
---
+++ scripts/eval.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+"""eval.py
+
+Usage:
+  eval.py --model=<model>  --data=<dataset> [--num-cores=<num-cores, --batch-size=<batch-size>, --gpu]
+
+Options:
+  -h --help         Show this screen.
+  --num-cores=<num-cores>  Use multiple-core processing [default: 4]
+  --batch-size=<batch-size>  Batch size [default: 32]
+"""
+
+from docopt import docopt
+from attacut import command, __version__, benchmark
+import json
+
+columns = [
+    "char_level:tp",
+    "char_level:fp",
+    "char_level:tn",
+    "char_level:fn",
+    "word_level:correctly_tokenised_words",
+    "word_level:total_words_in_sample",
+    "word_level:total_words_in_ref_sample",
+]
+
+def _read_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        lines = map(lambda r: r.strip(), f.readlines())
+    return list(lines)
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version=f"AttaCut: version {__version__}")
+
+    model_path = arguments["--model"]
+
+    src_file = arguments["--data"] + "/input.txt"
+
+    slug = arguments["--data"].split("/")[-1]
+    dest="%s/%s.txt" % (arguments["--model"], slug)
+
+    time_took = command.main(
+        src_file,
+        model_path,
+        int(arguments["--num-cores"]),
+        int(arguments["--batch-size"]),
+        device="cuda" if arguments["--gpu"] else "cpu",
+        dest=dest
+    )
+
+    # read tokenised back
+    actual = _read_file(dest)
+    label_file = arguments["--data"] + "/label.txt"
+    expected = _read_file(label_file)
+
+    df_raw = benchmark.benchmark(expected, actual)
+
+    statistics = dict()
+
+    for c in columns:
+        statistics[c] = float(df_raw[c].sum())
+
+    statistics["char_level:precision"] = statistics["char_level:tp"] / (
+        statistics["char_level:tp"] + statistics["char_level:fp"]
+    )
+
+    statistics["char_level:recall"] = statistics["char_level:tp"] / (
+        statistics["char_level:tp"] + statistics["char_level:fn"]
+    )
+
+    statistics["word_level:precision"] = statistics["word_level:correctly_tokenised_words"] \
+        / statistics["word_level:total_words_in_sample"]
+
+    statistics["word_level:recall"] = statistics["word_level:correctly_tokenised_words"] \
+        / statistics["word_level:total_words_in_ref_sample"]
+
+    statistics["time_took"] = time_took
+    # todo: stats
+    print(statistics)
+
+    stat_file = f"{model_path}/{slug}.json"
+
+    with open(stat_file, "w") as fh:
+        json.dump(statistics, fh)
